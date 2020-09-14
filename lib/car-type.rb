5f76574acc9274d20fa6d338136f54a62bceb847
Diego Lira <diegoalira@gmail.com>
2020-09-14 11:42:38 -0300

    Ask user car brand and type

---
+++ lib/car-type.rb
@@ -0,0 +1,16 @@
+class Crawler do
+  
+end
+
+class Crawler do
+  
+end
+
+class Car do
+
+  attr_reader: display_brands, display_models
+
+  def initialize(car_brand) do
+    @car_brand = car_brand
+  end
+end
