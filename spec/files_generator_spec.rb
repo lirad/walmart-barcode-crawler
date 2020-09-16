864058964448b7dd6cd413c6697e7b9239b03ea1
Diego Lira <diegoalira@gmail.com>
2020-09-16 17:55:26 -0300

    Correct tests for FileGenrator module

--- spec/files_generator_spec.rb
+++ spec/files_generator_spec.rb
@@ -1,7 +1,9 @@
 require('./lib/files_generator.rb')
 
+include FilesGenerator
+
 describe FilesGenerator do
-  let(:file_generator) { FilesGenerator.new }
+
   let(:crawled_products) do
     { 0 => { "ProductName": 'Antitranspirante Rexona men motion sense v8 en aerosol para caballero 90 g',
              "Price": '$47.00', "Size": '90',
@@ -10,7 +12,7 @@ describe FilesGenerator do
 
   describe '#table_generator' do
     it 'Check if tables are being correctly generated' do
-      generated_table = file_generator.table_generator(crawled_products)
+      generated_table = FilesGenerator.table_generator(crawled_products)
       expected_table = "+----------------------------------------------------------------------------+--------+------+--------+---------+\n| Product                                                                    | Price  | Size | Weight | Retail  |\n+----------------------------------------------------------------------------+--------+------+--------+---------+\n| Antitranspirante Rexona men motion sense v8 en aerosol para caballero 90 g | $47.00 | 90   | g      | Walmart |\n+----------------------------------------------------------------------------+--------+------+--------+---------+"
       expect(expected_table).to eq(generated_table.to_s)
     end
@@ -18,7 +20,7 @@ describe FilesGenerator do
 
   describe '#generate_output_csv' do
     it 'Check if files with products are being generated' do
-      file_generator.generate_output_csv(crawled_products)
+      FilesGenerator.generate_output_csv(crawled_products)
       expect(File.exist?('./output/products.csv')).to eq(true)
     end
   end
